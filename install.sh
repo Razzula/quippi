367ce586ccca4768b750ac93f1e6918c753b4bf9
Razzula <jack02gillespie@gmail.com>
2026-09-23 22:55:32 +0100

    Fix broken config

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@ sudo mkdir -p /etc/containers/systemd
 sudo cp quippi.container /etc/containers/systemd/
 sudo cp thwip.container /etc/containers/systemd/
 
-echo "Spinning up services..."
+echo "Reloading systemd..."
 sudo systemctl daemon-reload
 
+echo "Spinning up services..."
 sudo systemctl enable --now quippi.service
 sudo systemctl enable --now thwip.service
 
