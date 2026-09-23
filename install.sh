7e967e6846a7b65c9db7fb86e1564c64de2e20db
Razzula <jack02gillespie@gmail.com>
2026-09-23 22:58:08 +0100

    Update install script for generated services

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo "Reloading systemd..."
 sudo systemctl daemon-reload
 
 echo "Spinning up services..."
-sudo systemctl enable --now quippi.service
-sudo systemctl enable --now thwip.service
+sudo systemctl start quippi.service
+sudo systemctl start thwip.service
 
 echo "Installation complete."
